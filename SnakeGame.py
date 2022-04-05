40f3646727bb29a23fdfe7f9d92a2b8ff627b101
arorashivoy <85881270+arorashivoy@users.noreply.github.com>
2022-04-06 01:14:09 +0530

    created Class for Snake

--- SnakeGame.py
+++ SnakeGame.py
@@ -7,83 +7,82 @@ import pygame
 import random
 from enum import Enum
 
-""" Move snake with direction """
 
+class Snake:
+    def __init__(self):
+        self.positions = [[22, 22], ]  # Position of the body of the snake
+        self.dir = Directions.right
+        self.score = 1
 
-def MoveSnake():
-    global positions, dir
-    global execute, started, lost
-    global score
+    def MoveSnake(self):
+        """ Move snake with direction """
+        global execute, started, lost
 
-    x = positions[0][0]
-    y = positions[0][1]
+        x = self.positions[0][0]
+        y = self.positions[0][1]
 
-    # Moving snake
-    if dir == Directions.left:
-        pass
-        positions.insert(0, [x-1, y])
-        positions.pop(-1)
-    elif dir == Directions.right:
-        positions.insert(0, [x+1, y])
-        positions.pop(-1)
-    elif dir == Directions.up:
-        positions.insert(0, [x, y-1])
-        positions.pop(-1)
-    elif dir == Directions.down:
-        positions.insert(0, [x, y+1])
-        positions.pop(-1)
+        # Moving snake
+        if self.dir == Directions.left:
+            self.positions.insert(0, [x-1, y])
+            self.positions.pop(-1)
+        elif self.dir == Directions.right:
+            self.positions.insert(0, [x+1, y])
+            self.positions.pop(-1)
+        elif self.dir == Directions.up:
+            self.positions.insert(0, [x, y-1])
+            self.positions.pop(-1)
+        elif self.dir == Directions.down:
+            self.positions.insert(0, [x, y+1])
+            self.positions.pop(-1)
 
-    # Checking if snake is in border
-    if positions[0][0] == 45 or positions[0][0] == -1:
-        execute = False
-        started = False
-        lost = True
-        score = 1
-    elif positions[0][1] == 45 or positions[0][1] == -1:
-        execute = False
-        started = False
-        lost = True
-        score = 1
+        # Checking if snake is in border
+        if self.positions[0][0] == 45 or self.positions[0][0] == -1:
+            execute = False
+            started = False
+            lost = True
+            self.score = 1
+        elif self.positions[0][1] == 45 or self.positions[0][1] == -1:
+            execute = False
+            started = False
+            lost = True
+            self.score = 1
 
-    # Check if snake hit his own body
-    if positions[0] in positions[1:]:
-        execute = False
-        started = False
-        lost = True
-        score = 1
+        # Check if snake hit his own body
+        if self.positions[0] in self.positions[1:]:
+            execute = False
+            started = False
+            lost = True
+            self.score = 1
 
+    def IncSize(self):
+        """ Increase snake size when food is eaten """
 
-""" Increase snake size when food is eaten """
+        try:
+            x = self.positions[-1][0] + \
+                (self.positions[-1][0]-self.positions[-2][0])
+            y = self.positions[-1][1] + \
+                (self.positions[-1][1]-self.positions[-2][1])
 
+        # if the length of the snake is 1 (i.e. in the start)
+        except IndexError:
+            if self.dir == Directions.right:
+                x = self.positions[-1][0] - 1
+                y = self.positions[-1][1]
+            elif self.dir == Directions.left:
+                x = self.positions[-1][0] + 1
+                y = self.positions[-1][1]
+            elif self.dir == Directions.up:
+                x = self.positions[-1][0]
+                y = self.positions[-1][1] + 1
+            elif self.dir == Directions.down:
+                x = self.positions[-1][0]
+                y = self.positions[-1][1] - 1
 
-def IncSize():
-    global positions, dir
-    try:
-        x = positions[-1][0] + (positions[-1][0]-positions[-2][0])
-        y = positions[-1][1] + (positions[-1][1]-positions[-2][1])
-
-    # if the length of the snake is 1 (i.e. in the start)
-    except IndexError:
-        if dir == Directions.right:
-            x = positions[-1][0] - 1
-            y = positions[-1][1]
-        elif dir == Directions.left:
-            x = positions[-1][0] + 1
-            y = positions[-1][1]
-        elif dir == Directions.up:
-            x = positions[-1][0]
-            y = positions[-1][1] + 1
-        elif dir == Directions.down:
-            x = positions[-1][0]
-            y = positions[-1][1] - 1
-
-    positions.append([x, y])
-
-
-""" PyGame show snake and food when playing """
+        self.positions.append([x, y])
 
 
 def Show(positions, food):
+    """ PyGame show snake and food when playing """
     global x, y, width
     global scoreText
 
@@ -106,24 +105,19 @@ def Show(positions, food):
     win.blit(scoreText, (25, 500))
 
 
-""" Choose random food position """
-
-
-def FoodPos():
-    global positions
+def FoodPos(snake):
+    """ Choose random food position """
     x = random.randint(0, 44)
     y = random.randint(0, 44)
 
-    if [x, y] in positions:
-        x, y = FoodPos()
+    if [x, y] in snake.positions:
+        x, y = FoodPos(snake)
 
     return (x, y)
 
 
-""" Enum of the directions snake can move """
-
-
 class Directions(Enum):
+    """ Enum of the directions snake can move """
     right = 1
     left = 2
     up = 3
@@ -143,19 +137,19 @@ if __name__ == "__main__":
     y = 25
     width = 10
 
-    # initializing when nothing is pressed
+    # Setting snake object
+    snake = Snake()
+
     startText = titleFont.render("Press Space to Start", True, (42, 157, 143))
     resumeText = titleFont.render(
         "Press Space to Resume", True, (42, 157, 143))
 
+    # initializing when nothing is pressed
     win.fill((0, 0, 0))
     win.blit(startText, (130, 260))
     pygame.display.update()
 
-    positions = [[22, 22], ]  # Position of the body of the snake
-    dir = Directions.right
-    food = FoodPos()
-    score = 1
+    food = FoodPos(snake)
 
     execute = False
     run = True
@@ -173,13 +167,13 @@ if __name__ == "__main__":
                 if event.key == pygame.K_SPACE:
                     execute = not execute
                 elif event.key == pygame.K_UP or event.key == pygame.K_w:
-                    dir = Directions.up
+                    snake.dir = Directions.up
                 elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                    dir = Directions.down
+                    snake.dir = Directions.down
                 elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    dir = Directions.left
+                    snake.dir = Directions.left
                 elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    dir = Directions.right
+                    snake.dir = Directions.right
                 elif event.key == pygame.K_q:
                     execute = False
                     run = False
@@ -187,7 +181,7 @@ if __name__ == "__main__":
         # When lost
         if lost:
             win.fill((0, 0, 0))
-            Show(positions, food)
+            Show(snake.positions, food)
 
             win.blit(startText, (115, 250))
             pygame.display.update()
@@ -196,36 +190,36 @@ if __name__ == "__main__":
             # if the game has not started yet
             if not started:
                 scoreText = titleFont.render(
-                    "Score: {}".format(score), True, (42, 157, 143))
+                    "Score: {}".format(snake.score), True, (42, 157, 143))
             # Reseting new game values after losing
             if lost:
-                positions = [[22, 22], ]
-                dir = Directions.right
-                food = FoodPos()
+                snake.positions = [[22, 22], ]
+                snake.dir = Directions.right
+                food = FoodPos(snake)
 
             started = True
             lost = False
 
             pygame.time.delay(150)
 
-            MoveSnake()
+            snake.MoveSnake()
 
             # Food is eaten
-            if positions[0] == list(food):
-                score += 1
+            if snake.positions[0] == list(food):
+                snake.score += 1
                 scoreText = titleFont.render(
-                    "Score: {}".format(score), True, (42, 157, 143))
-                food = FoodPos()
-                IncSize()
+                    "Score: {}".format(snake.score), True, (42, 157, 143))
+                food = FoodPos(snake)
+                snake.IncSize()
 
-            Show(positions, food)
+            Show(snake.positions, food)
             pygame.display.update()
 
         # When game is paused
         else:
             if started:
                 win.fill((0, 0, 0))
-                Show(positions, food)
+                Show(snake.positions, food)
 
                 win.blit(resumeText, (115, 250))
                 pygame.display.update()
